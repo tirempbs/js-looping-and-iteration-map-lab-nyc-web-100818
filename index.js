deca478f99efdc3198d04edec234995248673421
tirempbs <tirem@fastmail.com>
2018-11-19 01:39:39 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file.
+function lowerCaseDrivers(array) {
+  return array.map(function(name) {return name.toLowerCase();});
+}
+
+function nameToAttributes(array) {
+  return array.map(
+    function(person) {
+      let name = person.split(' ');
+      return {firstName: name[0], lastName: name[1]}
+    }
+  );
+}
+
+function attributesToPhrase(array) {
+  return array.map(
+    function(object) {
+      return `${object.name} is from ${object.hometown}`
+    }
+  );
+}
